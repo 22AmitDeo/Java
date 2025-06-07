8087f7f41b4e5abfd1ca16b9184c880ad2e96a6c
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-07 23:01:12 +0530

    printing z in java

---
+++ printing_z.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class printing_z {
+    public static void main(String[] args){
+        System.out.println("*****");
+        System.out.println("   *  ");
+        System.out.println("  *   ");
+        System.out.println(" *    ");
+        System.out.println("*****");
+    }
+}
