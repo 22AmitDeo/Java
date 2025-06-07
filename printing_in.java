41a0682642dff80a609c4721697f615f00244e5d
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-07 22:54:33 +0530

    Printing in java

---
+++ printing_in.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class printing_in {
+    public static void main(String[] args) {
+        // Printing a string
+        System.out.print("Hello, World!\n"); // Prints with a newline at the end
+        System.out.println("Hello, World!"); // Prints with a newline at the end
+        System.out.print("Hello, World!");
+    }
+    
+}
