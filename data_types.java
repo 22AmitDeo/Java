6ce4e4818636b177074066772fad9fe9727d4610
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-08 23:54:47 +0530

    code

--- data_types.java
+++ data_types.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class data_types {
     public static void main(String[] args){
+            int x=25;
+            int y=10;
+            int sum=x+y;
+            System.out.print(sum);
             
     }
 }
