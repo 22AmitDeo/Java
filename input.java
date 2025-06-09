6c37256147652fc618c9fcb868eef337f01d4dde
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-09 23:06:58 +0530

    Taking Inputs

---
+++ input.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=Integer.parseInt(sc.nextLine());
+        String s=sc.nextLine();
+    }   
+}
