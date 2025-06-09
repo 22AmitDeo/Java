0165961e3ca5257d3016c16208a80f6a0e4ecf64
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-09 22:34:51 +0530

    bodmas

--- data_types.java
+++ data_types.java
@@ -5,6 +5,14 @@ public class data_types {
             int y=10;
             int sum=x+y;
             System.out.print(sum);
-            
+            int div=x/y;
+            int prd=x*y;
+            System.out.println(div);
+            System.out.println(prd);
+            System.out.println(div);
+            // Bodmas rule 
+            int a=(x+y)/(x*y);
+            System.out.println(a);
+
     }
 }
