1e96e8c45ccb10a8aae386c416364a81349f3852
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-09 22:43:40 +0530

    conditional statements

---
+++ conditional.java
@@ -0,0 +1,13 @@
+public class conditional {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 10;
+        if (x == y) {
+            System.out.println("x equa; than y");
+        } else if (x > y) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("x is less than y");
+        }
+    }
+}
