bf3b7bdd1da809abadd393a8b930b6b8df1fe976
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-06-09 23:00:19 +0530

    Loops

---
+++ Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+        // for loop
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+        }
+        // While Loop
+        int j = 0;
+        while (j < 4) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
